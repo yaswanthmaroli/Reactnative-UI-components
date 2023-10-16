6f4234c5333c517643e1fa4c6ffe2a9a6afa5a94
Yaswanth <yaswanth.m@infospica.com>
2023-10-16 13:38:21 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+// // React native core components
+
+// 1. View - The most fundamental building block for creating user interfaces in React Native. It's like a <div> in web development and can be used to group other components, apply styles, and structure your UI.
+// 2. Text -  : Used for displaying text content. It's similar to an HTML <span> or <p> element. You can apply styles to it just like any other component.
+// 3. TextInput - Allows users to input text. It's used for creating text input fields, similar to HTML's <input> elements.
+// 4. ScrollView -  Provides a scrollable view, which is essential for displaying content that doesn't fit entirely on the screen. You can nest other components inside it.
+// 5. FlatList - An efficient way to create lists. It's suitable for rendering large data sets efficiently, and it supports infinite scrolling.
+// 6. SectionList - A variation of FlatList that allows you to create lists with section headers.
+// 7. Image -  image tag is use for displaying images
+// 8. ImageBackground - Image background is used for display background images
+// 9. KeyboardAvoidingView - The KeyboardAvoidingView is a helpful component in React Native for ensuring that your UI adjusts when the on-screen keyboard is displayed, preventing it from covering important UI elements. It's especially useful for forms and input fields to maintain a user-friendly experience.
+// 10. TouchableWithoutFeedback -Provides no visual feedback when touched. It's mainly used when you want to capture touch events but don't need any visual indication.Useful for capturing touches on non-interactive elements.
+// 11. DrawerLayoutAndroid - DrawerLayout is an Android layout container that provides a convenient way to implement a navigation drawer in your Android app. A navigation drawer is a common user interface pattern that allows users to access app navigation or additional features by swiping or tapping to reveal a hidden panel, typically from the left or right side of the screen.
+// 12. TouchableNativeFeedback - This component is specific to Android and provides touch feedback using the native ripple effect. It's designed to give a more native feel to touch events on Android devices.
+// 13. SafeAreaView -SafeAreaView is a component in React Native that is used to ensure that your content is displayed within the safe area of a mobile device's screen. The safe area is the portion of the screen that is visible and not obstructed by notches, status bars, or other device-specific elements. SafeAreaView helps ensure that your UI elements are not obscured by these areas, providing a consistent and visually appealing layout.
+// 14. StatusBar - Used to customize the status bar at the top of the screen, including setting its background color and text color.
+// 15. Button - A simple button element that users can tap or press to trigger an action.
+// 16. Switch - A component that provides an on/off toggle switch.
+// 17. TouchableHighLight - The TouchableHighlight can be used where you would use a button or link on the web. The background of this component becomes dark on pressing it.
+// 18. TouchableOpacity - touchableOpacity are also touchable component that provides userfeedback when they are pressed . but it provides opactity when we pressed
+// 19. Modal -  A popup or dialog that can be displayed on top of the main content.
+// 20. RefreshControl - RefreshControl is a component in React Native that provides a pull-to-refresh functionality for scrollable views, such as ScrollView, FlatList, and SectionList. It allows users to refresh the content of the view by pulling down and releasing, which triggers a refreshing action. This component is commonly used for implementing a refresh mechanism in lists or other scrollable content.
+// 21. pressable -Pressable is a touchable component in React Native that provides a flexible and accessible way to handle touch interactions. It is introduced as an alternative to traditional touchable components like TouchableOpacity and TouchableHighlight and is designed to support various user interactions, including pressing, hovering, and more.
+// 22. VirtualizedList -s the underlying implementation used by both FlatList and SectionList. It’s a high-performance list component that efficiently renders only the items currently visible on the screen, regardless of the size of the data set. VirtualizedList is the recommended choice when you need fine-grained control over list rendering and want to create a custom list component.
+// 23. ActivityIndicator -  ActivityIndicator is used for showing loader
+// 24. InputAccessoryView - An Input Accessory View is a custom view that you can add to the keyboard when a user interacts with a text input field on iOS. It appears as an additional toolbar or view above the keyboard and is often used to provide extra functionality, shortcuts, or actions relevant to the input field.
